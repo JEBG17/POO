3429add5b73b9ee8d661ba72166045891c920306
JEBG17 <jesusmdo1012@gmail.com>
2020-01-30 15:42:34 -0700

    Primer solucion

---
+++ MiPrmeraSolucion/MiPrmerProyectoConsola/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrmerProyectoConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C;
+            Console.Write("Introduce un número: ");
+            A = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Itroduce otro número: ");
+            B = Convert.ToInt32(Console.ReadLine());
+            C = A + B;
+
+            Console.WriteLine("La suma de los numeros es: " + C);
+            Console.ReadKey();
+        }
+    }
+}
