8172b87fd5679ef35f0c29bb2614ce817f161aa0
JEBG17 <jesusmdo1012@gmail.com>
2020-02-08 12:07:55 -0700

    primera unidad

---
+++ Unidad1POO/Embotelladora/ClassBotella.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Embotelladora
+{
+    class ClassBotella
+    {
+        //atributos de la  clase 
+        public int nivelLlenado;
+        int mililitrosVerificados;
+        //metodos de la clse 
+       public String verifiarLlenado()
+        {
+            string mensaje;
+            if (nivelLlenado == 250)
+                mensaje = "Llenado perfecto";
+            else if (nivelLlenado < 250)
+            {
+                mililitrosVerificados = 250 - nivelLlenado;
+                mensaje = "Le falta " + mililitrosVerificados + " mililitros ";
+            }
+            else
+            {
+                mililitrosVerificados = nivelLlenado - 250;
+                mensaje = "Le sobra" + mililitrosVerificados + " mililitros ";
+            }
+
+
+
+
+            return mensaje ;
+        }
+    }
+}
