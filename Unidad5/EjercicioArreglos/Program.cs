996fe94ed0fa0bc95ddc664c3132098d6cfca95e
JEBG17 <jesusmdo1012@gmail.com>
2020-04-01 15:05:38 -0600

    unidad 5

---
+++ Unidad5/EjercicioArreglos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioArreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int suma=0;
+            double Promedio;
+            //Arreglos 
+            int[] calificaciones = { 9, 8, 6, 10, 7 };
+            string[] Alumnos = { "Ernesto", "Angel", "Jesus", "Ivan", "Leti" };
+
+            for(i=0; i < calificaciones.Length; i++)
+            {
+                Console.WriteLine("La nota de {0} es {1}", Alumnos[i], calificaciones[i]);
+            }
+           for(i=0;i<calificaciones.Length; i++)
+            {
+                suma = suma + calificaciones[i];
+             
+            }
+            Promedio = suma / calificaciones.Length;
+            Console.WriteLine("...........................");
+            Console.WriteLine("La suma detodad las calificaciones es: {0}", suma);
+            Console.WriteLine("El promedio de las calificaciones es: {0}", Promedio);
+
+            Console.ReadLine();
+
+        }
+    }
+}
