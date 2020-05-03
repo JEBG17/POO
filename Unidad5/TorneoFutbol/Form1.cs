2056eeffea02a46f7cb782cbf8a378c4c818b0e8
JEBG17 <jesusmdo1012@gmail.com>
2020-05-03 02:30:57 -0600

    unidad 5

---
+++ Unidad5/TorneoFutbol/Form1.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TorneoFutbol
+{
+	public partial class Form1 : Form
+	{
+        Torneo objTorneo;
+		int equipo, partidos, E, i, s;
+		int suma, mayor=0;
+		
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnImprimir_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Ganó con "+mayor+ " puntos ");
+			btnAceptar.Enabled = true;
+			txtNombreTorneo.Enabled = true;
+			nudEquipos.Enabled = true;
+			btnImprimir.Enabled = false;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			
+
+		}
+
+		private void btnAceptar_Click(object sender, EventArgs e)
+        {
+			if( txtNombreTorneo.Text=="")
+			{
+				errorProvider1.SetError(txtNombreTorneo, "Debe de ingresar un nombre");
+				txtNombreTorneo.Focus();
+				return;
+			}
+			errorProvider1.SetError(txtNombreTorneo, "");
+
+
+            objTorneo = new Torneo(); 
+            objTorneo.NumeroEquipos = (int)nudEquipos.Value;
+			equipo = objTorneo.NumeroEquipos;
+            objTorneo.NombreTorneo = txtNombreTorneo.Text;
+			objTorneo.NumeroPartidosEquipos = objTorneo.NumeroEquipos - 1;
+			partidos = objTorneo.NumeroPartidosEquipos;
+			objTorneo.PuntosXPartidos = new int[equipo, partidos];
+			objTorneo.NombreDeEquipos = new string[equipo];
+			
+			for (E=0; E <equipo; E++)
+			{
+				objTorneo.NombreDeEquipos[E] = Interaction.InputBox("Introduce el nombre del equipo ");
+			}
+
+			if (E == equipo)
+			{
+				E = 0;
+				for ( i = 0; i < equipo; i++)
+			    {
+
+				   for (int j = 0; j < partidos; j++)
+				   {
+					 objTorneo.PuntosXPartidos[i , j] = Convert.ToInt16(Interaction.InputBox("introduce los puntos del equipo "+objTorneo.NombreDeEquipos[E] + " del partido " + i +" , "+ j));
+
+				   }
+					E++;
+			    }
+
+			}
+			if (i == equipo)
+			{
+				objTorneo.SumaPuntaje = new int[equipo];
+
+				for (i = 0; i < equipo; i++ )
+				{
+					suma = 0;
+					for(int j=0; j < partidos; j++)
+					{
+						suma = suma + objTorneo.PuntosXPartidos[i,j];	
+					}
+					objTorneo.SumaPuntaje[s]= suma;
+
+					if (objTorneo.SumaPuntaje[s]> mayor)
+					{
+					
+						mayor = objTorneo.SumaPuntaje[s];
+					}
+					s++;
+				}
+
+			}
+			txtNombreTorneo.Clear();
+			nudEquipos.Value = 1;
+			txtNombreTorneo.Enabled = false;
+			nudEquipos.Enabled = false;
+			btnAceptar.Enabled = false;
+			btnImprimir.Enabled = true;
+
+
+			
+			
+			
+        }
+
+    }
+}
---
+++ Unidad5/TorneoFutbol/Torneo.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorneoFutbol
+{
+	class Torneo
+	{
+		public string NombreTorneo { get; set; }
+		public int NumeroEquipos { get; set; }
+		public int NumeroPartidosEquipos { get; set; }
+		public string FechaInicio { get; set; }
+		public string FechaFin { get; set; }
+		//Arreglo Multidimensional
+		public int[,] PuntosXPartidos;
+		//Arreglo Unidemencional
+		public int[] SumaPuntaje;
+		public string[] NombreDeEquipos;
+
+		//Construcutor de la clase 
+		public Torneo ()
+		{
+			NombreTorneo = "";
+			NumeroEquipos = 0;
+			NumeroPartidosEquipos = 0;
+			FechaInicio = "";
+			FechaFin = "";
+
+		}
+
+	
+	}
+}
